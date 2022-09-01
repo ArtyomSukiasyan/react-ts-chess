68e00978b7eaf4d3b5cac2bfbc2e151fcc66b20a
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 11:12:24 +0400

    feat: renamed clearPossibleHighlight file

--- src/Chess/helpers/clearPossibleHighlight.ts
+++ src/Chess/helpers/clearPossibleHighlight.ts
@@ -1,9 +1,11 @@
-export default function clearPossibleHighlight(squares) {
+export default function clearPossibleHighlight(squares: any[]): any[] {
   const copySquares = squares.slice();
+
   for (let i = 0; i < 64; i++) {
     if (copySquares[i].possible){
        copySquares[i].possible = false;
     }
   }
+  
   return copySquares;
 }
