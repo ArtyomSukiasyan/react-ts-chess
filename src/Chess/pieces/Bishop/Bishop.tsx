1104226f35e8219520ac5e54c93b917a22e4a717
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 12:55:17 +0400

    feat: renamed Bishop piece

--- src/Chess/pieces/Bishop/Bishop.tsx
+++ src/Chess/pieces/Bishop/Bishop.tsx
@@ -3,7 +3,13 @@ import { whiteBishop, blackBishop } from "../../constants/asciis";
 import styles from "../../Game.module.css";
 
 export default class Bishop {
-  constructor(player) {
+  player: any;
+  highlight: boolean;
+  possible: boolean;
+  icon: any;
+  ascii: any;
+
+  constructor(player: any) {
     this.player = player;
     this.highlight = false;
     this.possible = false;
@@ -17,7 +23,7 @@ export default class Bishop {
     this.ascii = player === white ? whiteBishop : blackBishop;
   }
 
-  canMove(start, end) {
+  canMove(start: number, end: number): boolean {
     const startRow = 8 - Math.floor(start / 8);
     const startCol = (start % 8) + 1;
     const endRow = 8 - Math.floor(end / 8);
