2cea351d7c4c175aadc3b2ec2f095fd949ec5c3d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 11:08:34 +0400

    feat: renamed canEnpassant file

--- src/Chess/helpers/canEnpassant.ts
+++ src/Chess/helpers/canEnpassant.ts
@@ -1,7 +1,7 @@
 import { blackPawn, whitePawn } from "../constants/asciis";
 import { black, white } from "../constants/players";
 
-export default function canEnpassant(start, end, squares, statePassantPos, passantPos) {
+export default function canEnpassant(start: number, end: number, squares: any[], statePassantPos: number, passantPos: number): boolean {
     let passant = passantPos === undefined ? statePassantPos : passantPos;
     let startRow = 8 - Math.floor(start / 8);
     let startCol = (start % 8) + 1;
@@ -17,9 +17,11 @@ export default function canEnpassant(start, end, squares, statePassantPos, passa
       if (copySquares[start].player === black && (start < 8 || start > 15))
         return false;
     }
+
     if (copySquares[end].ascii !== null) {
       if (colDiff === 0) return false;
     }
+    
     if (rowDiff === 1 && colDiff === 1) {
       if (copySquares[end].ascii === null) {
         if (copySquares[start + 1].ascii !== blackPawn || passant !== start + 1)
