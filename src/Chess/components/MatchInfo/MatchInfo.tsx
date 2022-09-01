9c3aa6309249583d198835cf8c911931b8038d8d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 13:28:40 +0400

    feat: make constants for matchInfo

--- src/Chess/components/MatchInfo/MatchInfo.tsx
+++ src/Chess/components/MatchInfo/MatchInfo.tsx
@@ -1,7 +1,14 @@
 import { ReactElement } from "react";
+import Button from "./Button";
+import {
+  backAtwText,
+  backText,
+  nextAtwText,
+  nextText,
+  restart,
+} from "../../constants/matchInfo";
 import { IMatchInfo } from "../../models/Matchinfo";
 import styles from "../../Game.module.css";
-import Button from "./Button";
 
 export default function MatchInfo({
   backAtw,
@@ -13,11 +20,11 @@ export default function MatchInfo({
   return (
     <div className={styles.side_box}>
       <div className={styles.button_wrapper}>
-        <Button text="&lt;&lt;" onClick={backAtw} />
-        <Button text="&lt;" onClick={back} />
-        <Button text="Restart" onClick={reset} />
-        <Button text="&gt;" onClick={next} />
-        <Button text="&gt;&gt;" onClick={nextAtw} />
+        <Button text={backAtwText} onClick={backAtw} />
+        <Button text={backText} onClick={back} />
+        <Button text={restart} onClick={reset} />
+        <Button text={nextText} onClick={next} />
+        <Button text={nextAtwText} onClick={nextAtw} />
       </div>
     </div>
   );
---
+++ src/Chess/constants/matchInfo.ts
@@ -0,0 +1,9 @@
+export const backAtwText = "<<";
+
+export const backText = "<";
+
+export const restart = "Restart";
+
+export const nextText = ">";
+
+export const nextAtwText = ">>";
