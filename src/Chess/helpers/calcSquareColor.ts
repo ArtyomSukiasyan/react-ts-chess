d27a4677074e57e8c64922bd5fb5547691edb097
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 11:05:45 +0400

    feat: renaed calcSquareColor file

--- src/Chess/helpers/calcSquareColor.ts
+++ src/Chess/helpers/calcSquareColor.ts
@@ -1,28 +1,37 @@
 import { whiteKing } from "../constants/asciis";
 import { blackKing } from "../constants/asciis";
 
-export default function calcSquareColor(i, j, squares) {
+export default function calcSquareColor(
+  i: number,
+  j: number,
+  squares: any[]
+): string {
   const isKing =
     squares[i * 8 + j].ascii === whiteKing ||
     squares[i * 8 + j].ascii === blackKing;
+
   let squareColor =
     (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))
       ? "white_square"
       : "black_square";
+
   if (squares[i * 8 + j].highlight) {
     squareColor = "selected_square";
   }
+
   if (squares[i * 8 + j].possible) {
     squareColor = "highlighted_square";
   }
+
   if (squares[i * 8 + j].ascii !== null && isKing) {
     if (squares[i * 8 + j].inCheck) {
       squareColor = "in_check_square";
     }
   }
 
-  function isEven(value) {
+  function isEven(value: number): number {
     return value % 2;
   }
+
   return squareColor;
 }
