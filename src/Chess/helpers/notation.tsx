8baa6bcb4b3ab3fddf1ec6905f47584b8b86dee2
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 11:20:44 +0400

    feat: renamed notation file

--- src/Chess/helpers/notation.tsx
+++ src/Chess/helpers/notation.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import styles from "../Game.module.css";
 
-export default function Notation(props) {
+export default function Notation(props): any {
   return <button className={styles.label}> {props.value} </button>;
 }
