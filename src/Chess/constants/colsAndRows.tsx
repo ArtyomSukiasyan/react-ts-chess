651c6fdeb9b66ca492aa79d63b4793a612c39587
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 13:57:35 +0400

    feat: rewrite and add types for colsAndRous

--- src/Chess/constants/colsAndRows.tsx
+++ src/Chess/constants/colsAndRows.tsx
@@ -1,42 +1,14 @@
+import { ReactElement } from "react";
 import Notation from "../helpers/notation";
 
-export const rowNums: any = [];
+export const rowNums: ReactElement[] = [];
 for (let i = 8; i > 0; i--) {
   rowNums.push(<Notation key={i} value={i} />);
 }
 
-export const colNums: any = [];
-for (let i = 1; i < 9; i++) {
-  let letter;
+export const colNums: ReactElement[] = [];
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
-  switch (i) {
-    case 1:
-      letter = "A";
-      break;
-    case 2:
-      letter = "B";
-      break;
-    case 3:
-      letter = "C";
-      break;
-    case 4:
-      letter = "D";
-      break;
-    case 5:
-      letter = "E";
-      break;
-    case 6:
-      letter = "F";
-      break;
-    case 7:
-      letter = "G";
-      break;
-    case 8:
-      letter = "H";
-      break;
-    default:
-      break;
-  }
-
-  colNums.push(<Notation key={letter} value={letter} />);
+for (let i = 0; i < letters.length; i++) {
+  colNums.push(<Notation key={letters[i]} value={letters[i]} />);
 }
