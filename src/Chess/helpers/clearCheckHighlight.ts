3c0d04894deabca88c8c7bcbc477e14797b4322b
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-01 11:11:10 +0400

    feat: renamed clearCheckHighlight file

--- src/Chess/helpers/clearCheckHighlight.ts
+++ src/Chess/helpers/clearCheckHighlight.ts
@@ -1,8 +1,12 @@
 import { white } from "../constants/players";
 import { whiteKing, blackKing } from "../constants/asciis";
 
-export default function clearCheckHighlight(squares, player) {
+export default function clearCheckHighlight(
+  squares: any[],
+  player: "white" | "black"
+): any[] | void {
   const copySquares = squares.slice();
+  
   for (let j = 0; j < 64; j++) {
     if (copySquares[j].ascii === (player === white ? whiteKing : blackKing)) {
       copySquares[j].inCheck = false;
