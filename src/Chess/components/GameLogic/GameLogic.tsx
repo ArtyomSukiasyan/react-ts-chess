3cb4cc990948b66c4df01a09fe436ee805d3cbbf
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-05 13:25:07 +0400

    feat: make makeMove helper function

--- src/Chess/components/GameLogic/GameLogic.tsx
+++ src/Chess/components/GameLogic/GameLogic.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import FillerPiece from "../../pieces/FillerPiece/FillerPiece";
 import Queen from "../../pieces/Queen/Queen";
-import Rook from "../../pieces/Rook/Rook";
-import Bishop from "../../pieces/Bishop/Bishop";
-import Knight from "../../pieces/Knight/Knight";
 import MatchInfo from "../MatchInfo/MatchInfo";
 import clearHighlight from "../../helpers/clearHighlight";
 import clearPossibleHighlight from "../../helpers/clearPossibleHighlight";
@@ -26,6 +23,7 @@ import styles from "../../Game.module.css";
 import MakeBoard from "../../helpers/makeBoard";
 import checkStaleMate from "../../helpers/checkStaleMate";
 import checkInvalidMove from "../../helpers/checkInvalidMove";
+import makeMove from "../../helpers/makeMove";
 
 export default function Board(): any {
   const [squares, setSquares] = useState<any>(initializeBoard());
@@ -116,7 +114,7 @@ export default function Board(): any {
       }
     }
 
-    copySquares = makeMove(copySquares, start, end);
+    copySquares = makeMove(copySquares, start, end, passantPosition);
 
     let passantTrue =
       player === white
@@ -192,77 +190,6 @@ export default function Board(): any {
     setMated(checkMated || staleMated ? true : false);
     setTurn(player === black ? white : black);
     setTrueTurn(player === black ? white : black);
-  };
-
-  const makeMove = (squares: any, start: any, end: any, passantPos?: any) => {
-    const copySquares = squares.slice();
-    let isKing =
-      copySquares[start].ascii === whiteKing ||
-      copySquares[start].ascii === blackKing;
-    if (isKing && Math.abs(end - start) === 2) {
-      if (end === (copySquares[start].ascii === whiteKing ? 62 : 6)) {
-        copySquares[end - 1] = copySquares[end + 1];
-        copySquares[end - 1].highlight = true;
-        copySquares[end + 1] = new FillerPiece(null);
-        copySquares[end + 1].highlight = true;
-      } else if (end === (copySquares[start].ascii === whiteKing ? 58 : 2)) {
-        copySquares[end + 1] = copySquares[end - 2];
-        copySquares[end + 1].highlight = true;
-        copySquares[end - 2] = new FillerPiece(null);
-        copySquares[end - 2].highlight = true;
-      }
-    }
-
-    let passant = passantPos == null ? passantPosition : passantPos;
-    if (
-      copySquares[start].ascii === whitePawn ||
-      copySquares[start].ascii === blackPawn
-    ) {
-      if (end - start === -7 || end - start === 9) {
-        if (start + 1 === passant)
-          copySquares[start + 1] = new FillerPiece(null);
-      } else if (end - start === -9 || end - start === 7) {
-        if (start - 1 === passant)
-          copySquares[start - 1] = new FillerPiece(null);
-      }
-    }
-
-    copySquares[end] = copySquares[start];
-    copySquares[end].highlight = true;
-    copySquares[start] = new FillerPiece(null);
-    copySquares[start].highlight = true;
-
-    if (copySquares[end].ascii === whitePawn && end >= 0 && end <= 7) {
-      const figure = prompt(
-        "please, select 'queen', 'rook', 'bishop' or 'knight'"
-      );
-      if (figure === "queen") {
-        copySquares[end] = new Queen(white);
-      } else if (figure === "rook") {
-        copySquares[end] = new Rook(white);
-      } else if (figure === "bishop") {
-        copySquares[end] = new Bishop(white);
-      } else if (figure === "knight") {
-        copySquares[end] = new Knight(white);
-      }
-      copySquares[end].highlight = true;
-    }
-    if (copySquares[end].ascii === blackPawn && end >= 56 && end <= 63) {
-      const figure = prompt(
-        "please, select 'queen', 'rook', 'bishop' or 'knight'"
-      );
-      if (figure === "queen") {
-        copySquares[end] = new Queen(black);
-      } else if (figure === "rook") {
-        copySquares[end] = new Rook(black);
-      } else if (figure === "bishop") {
-        copySquares[end] = new Bishop(black);
-      } else if (figure === "knight") {
-        copySquares[end] = new Knight(black);
-      }
-      copySquares[end].highlight = true;
-    }
-    return copySquares;
   };
 
   const isMoveAvailable = (
---
+++ src/Chess/helpers/makeMove.ts
@@ -0,0 +1,87 @@
+import {
+  blackKing,
+  blackPawn,
+  whiteKing,
+  whitePawn,
+} from "../constants/asciis";
+import { black, white } from "../constants/players";
+import Bishop from "../pieces/Bishop/Bishop";
+import FillerPiece from "../pieces/FillerPiece/FillerPiece";
+import Knight from "../pieces/Knight/Knight";
+import Queen from "../pieces/Queen/Queen";
+import Rook from "../pieces/Rook/Rook";
+
+export default function makeMove(
+  squares: any,
+  start: number,
+  end: number,
+  passantPosition: number,
+  passantPos?: number
+) {
+  const copySquares = squares.slice();
+  let isKing =
+    copySquares[start].ascii === whiteKing ||
+    copySquares[start].ascii === blackKing;
+  if (isKing && Math.abs(end - start) === 2) {
+    if (end === (copySquares[start].ascii === whiteKing ? 62 : 6)) {
+      copySquares[end - 1] = copySquares[end + 1];
+      copySquares[end - 1].highlight = true;
+      copySquares[end + 1] = new FillerPiece(null);
+      copySquares[end + 1].highlight = true;
+    } else if (end === (copySquares[start].ascii === whiteKing ? 58 : 2)) {
+      copySquares[end + 1] = copySquares[end - 2];
+      copySquares[end + 1].highlight = true;
+      copySquares[end - 2] = new FillerPiece(null);
+      copySquares[end - 2].highlight = true;
+    }
+  }
+
+  let passant = passantPos == null ? passantPosition : passantPos;
+  if (
+    copySquares[start].ascii === whitePawn ||
+    copySquares[start].ascii === blackPawn
+  ) {
+    if (end - start === -7 || end - start === 9) {
+      if (start + 1 === passant) copySquares[start + 1] = new FillerPiece(null);
+    } else if (end - start === -9 || end - start === 7) {
+      if (start - 1 === passant) copySquares[start - 1] = new FillerPiece(null);
+    }
+  }
+
+  copySquares[end] = copySquares[start];
+  copySquares[end].highlight = true;
+  copySquares[start] = new FillerPiece(null);
+  copySquares[start].highlight = true;
+
+  if (copySquares[end].ascii === whitePawn && end >= 0 && end <= 7) {
+    const figure = prompt(
+      "please, select 'queen', 'rook', 'bishop' or 'knight'"
+    );
+    if (figure === "queen") {
+      copySquares[end] = new Queen(white);
+    } else if (figure === "rook") {
+      copySquares[end] = new Rook(white);
+    } else if (figure === "bishop") {
+      copySquares[end] = new Bishop(white);
+    } else if (figure === "knight") {
+      copySquares[end] = new Knight(white);
+    }
+    copySquares[end].highlight = true;
+  }
+  if (copySquares[end].ascii === blackPawn && end >= 56 && end <= 63) {
+    const figure = prompt(
+      "please, select 'queen', 'rook', 'bishop' or 'knight'"
+    );
+    if (figure === "queen") {
+      copySquares[end] = new Queen(black);
+    } else if (figure === "rook") {
+      copySquares[end] = new Rook(black);
+    } else if (figure === "bishop") {
+      copySquares[end] = new Bishop(black);
+    } else if (figure === "knight") {
+      copySquares[end] = new Knight(black);
+    }
+    copySquares[end].highlight = true;
+  }
+  return copySquares;
+}
