66ae757c6ab7cc3c7a7be555f46aeb65ad683529
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-06 18:37:18 +0400

    feat: add types for and code refactor on makeBoard helper function

--- src/Chess/helpers/makeBoard.tsx
+++ src/Chess/helpers/makeBoard.tsx
@@ -2,80 +2,70 @@ import calcSquareColor from "./calcSquareColor";
 import Square from "../components/Squares/Squares";
 
 import styles from "../Game.module.css";
+import { IPiece } from "../models/Piece";
+import { ReactElement } from "react";
 
 export default function MakeBoard(
   isWhite: boolean,
-  squares: any,
-  turn: any,
+  squares: IPiece[],
+  turn: string,
   mated: boolean,
   historyNum: any,
-  turnNum: any,
-  handleClick: any
+  turnNum: number,
+  handleClick: Function
 ) {
   const board = [];
 
   if (isWhite) {
     for (let i = 0; i < 8; i++) {
-      const squareRows = [];
+      const squareRows: ReactElement[] = [];
+
       for (let j = 0; j < 8; j++) {
-        const copySquares = squares.slice();
-        let squareColor = calcSquareColor(i, j, copySquares);
-        let squareCursor = styles.pointer;
-        if (copySquares[i * 8 + j].player !== turn) {
-          squareCursor = styles.default;
-        }
-
-        if (mated) {
-          squareCursor = styles.default;
-        }
-
-        if (historyNum - 1 !== turnNum) {
-          squareCursor = styles.not_allowed;
-        }
-
-        squareRows.push(
-          <Square
-            value={copySquares[i * 8 + j]}
-            color={squareColor}
-            cursor={squareCursor}
-            onClick={() => handleClick(i * 8 + j)}
-            key={i * 8 + j}
-          />
-        );
+        const squareRow = makeSquareRow(i, j);
+        squareRows.push(squareRow);
       }
+
       board.push(<div key={i + 64}>{squareRows}</div>);
     }
   } else {
     for (let i = 7; i >= 0; i--) {
-      const squareRows = [];
+      const squareRows: ReactElement[] = [];
+
       for (let j = 7; j >= 0; j--) {
-        const copySquares = squares.slice();
-        let squareColor = calcSquareColor(i, j, copySquares);
-        let squareCursor = styles.pointer;
-        if (copySquares[i * 8 + j].player !== turn) {
-          squareCursor = styles.default;
-        }
-
-        if (mated) {
-          squareCursor = styles.default;
-        }
-
-        if (historyNum - 1 !== turnNum) {
-          squareCursor = styles.not_allowed;
-        }
-
-        squareRows.push(
-          <Square
-            value={copySquares[i * 8 + j]}
-            color={squareColor}
-            cursor={squareCursor}
-            onClick={() => handleClick(i * 8 + j)}
-            key={i * 8 + j}
-          />
-        );
+        const squareRow = makeSquareRow(i, j);
+        squareRows.push(squareRow);
       }
+
       board.push(<div key={i + 64}>{squareRows}</div>);
     }
+  }
+
+  function makeSquareRow(i: number, j: number): ReactElement {
+    const copySquares = squares.slice();
+    let squareColor = calcSquareColor(i, j, copySquares);
+    let squareCursor = styles.pointer;
+
+    if (copySquares[i * 8 + j].player !== turn) {
+      squareCursor = styles.default;
+    }
+
+    if (mated) {
+      squareCursor = styles.default;
+    }
+
+    if (historyNum - 1 !== turnNum) {
+      squareCursor = styles.not_allowed;
+    }
+
+    return (
+      <Square
+        value={copySquares[i * 8 + j]}
+        color={squareColor}
+        cursor={squareCursor}
+        onClick={() => handleClick(i * 8 + j)}
+        key={i * 8 + j}
+      />
+    );
   }
 
   return board;
