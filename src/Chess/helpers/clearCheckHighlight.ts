d9d8bb642f2d66e52fc005a44595406da728c671
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-06 18:16:33 +0400

    feat: add types for check clearHighliht file

--- src/Chess/helpers/clearCheckHighlight.ts
+++ src/Chess/helpers/clearCheckHighlight.ts
@@ -1,15 +1,18 @@
 import { white } from "../constants/players";
 import { whiteKing, blackKing } from "../constants/asciis";
+import { IPiece } from "../models/Piece";
 
 export default function clearCheckHighlight(
-  squares: any,
-  player: any
-): any[] | void {
+  squares: IPiece[],
+  player: string
+): IPiece[] | void {
   const copySquares = squares.slice();
-  
+  const kingColor = player === white ? whiteKing : blackKing;
+
   for (let j = 0; j < 64; j++) {
-    if (copySquares[j].ascii === (player === white ? whiteKing : blackKing)) {
+    if (copySquares[j].ascii === kingColor) {
       copySquares[j].inCheck = false;
+      
       return copySquares;
     }
   }
