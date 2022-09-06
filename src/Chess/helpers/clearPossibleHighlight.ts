4d380ea77b94ab795601b2ea10fc3e82b22b164c
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-06 18:17:23 +0400

    feat: add types for clearPossibleHighliht file

--- src/Chess/helpers/clearPossibleHighlight.ts
+++ src/Chess/helpers/clearPossibleHighlight.ts
@@ -1,11 +1,13 @@
-export default function clearPossibleHighlight(squares: any): any[] {
+import { IPiece } from "../models/Piece";
+
+export default function clearPossibleHighlight(squares: IPiece[]): IPiece[] {
   const copySquares = squares.slice();
 
   for (let i = 0; i < 64; i++) {
-    if (copySquares[i].possible){
-       copySquares[i].possible = false;
+    if (copySquares[i].possible) {
+      copySquares[i].possible = false;
     }
   }
-  
+
   return copySquares;
 }
