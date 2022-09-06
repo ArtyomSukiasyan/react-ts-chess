69b96b2e8f7b10d9e32f47c5c65292114cb2a9a6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-09-06 17:58:59 +0400

    feat: make model for castling conditions

---
+++ src/Chess/models/CastlingConditions.ts
@@ -0,0 +1,8 @@
+export interface ICastlingConditions {
+  whiteKingHasMoved: boolean;
+  blackKingHasMoved: boolean;
+  leftBlackRookHasMoved: boolean;
+  rightBlackRookHasMoved: boolean;
+  leftWhiteRookHasMoved: boolean;
+  rightWhiteRookHasMoved: boolean;
+}
